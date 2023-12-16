0bfdf7cc3afe87e03fcf7867866359e20fcd1709
devgasps <60119170+devgasps@users.noreply.github.com>
2023-12-16 09:28:34 -0500

    Delete assets directory

--- assets/javascript/mute.js
+++
@@ -1,8 +0,0 @@
-function toggleMute(event) {
-  var video = document.getElementById("audio");
-  if (event.target.id !== "copyText" && !event.target.closest("#alert-container") && !event.target.closest(".event-alert-box")) {
-    video.muted = !video.muted;
-  }
-}
-
-document.body.addEventListener("click", toggleMute);
